b747cece1bed4294e54586182e1d8a4d44b6cc77
mascpro-dev <marceloconelheiros@conexoes.digital>
2026-01-17 10:00:24 -0300

    analicrtics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+// 1. Importar o Analytics
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "MASC PRO",
-  description: "Plataforma Exclusiva para Profissionais.",
-};
+  title: 'MASC PRO',
+  description: 'Plataforma de Ensino Profissional',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br" className="dark">
-      <body className={`${inter.className} bg-slate-950 text-slate-50 antialiased`}>
+    <html lang="pt-BR">
+      <body className={inter.className}>
         {children}
+        {/* 2. Ativar o componente aqui no final */}
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
