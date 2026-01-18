4dae88c552222b8550d1a33f5db8730771ae663b
mascpro-dev <marceloconelheiros@conexoes.digital>
2026-01-18 00:22:05 -0300

    2

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
+// SÓ PODE TER ESSE IMPORT AQUI:
 import Sidebar from "@/componentes/Sidebar"; 
-// Não precisa mais importar MobileBottomNav aqui!
 
 export default function MainLayout({
   children,
@@ -9,11 +9,10 @@ export default function MainLayout({
   return (
     <div className="flex h-screen bg-black overflow-hidden relative">
       
-      {/* O Sidebar agora cuida do PC (lado) e do Celular (baixo) sozinho */}
+      {/* O Sidebar agora carrega tudo sozinho */}
       <Sidebar />
 
       <main className="flex-1 overflow-y-auto overflow-x-hidden relative scroll-smooth">
-        {/* Espaço extra embaixo no mobile (pb-28) para o menu não tapar nada */}
         <div className="p-6 pb-28 md:p-12 md:pb-12 max-w-7xl mx-auto">
           {children}
         </div>
